35dd6b96144a384b8720172154cf15ca941df382
BenjamimCS <uj1kjfomy@mozmail.com>
2023-03-21 03:12:41 -0400

    Set specific tabs for Yaml files

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -4,3 +4,11 @@ vim.o.expandtab  = true
 vim.o.number     = true
 vim.o.wrap       = "false"
 vim.o.cmdheight  = 2
+
+vim.cmd[[
+augroup yaml_tabs
+  autocmd!
+  autocmd BufNewFile,BufRead,FileType,VimEnter,BufEnter
+  \ *.yml,*.yaml setlocal tabstop=3 | setlocal shiftwidth=3
+augroup END
+]]
