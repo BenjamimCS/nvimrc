f4b5c02c20cee4a1dcca96a9002b0d9ff3f6d977
BenjamimCS <uj1kjfomy@mozmail.com>
2023-03-21 02:58:33 -0400

    Align assignments

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -1,5 +1,5 @@
-vim.o.tabstop = 2
+vim.o.tabstop    = 2
 vim.o.shiftwidth = 2
-vim.o.expandtab = true
-vim.o.number = true
-vim.o.wrap = "false"
+vim.o.expandtab  = true
+vim.o.number     = true
+vim.o.wrap       = "false"
