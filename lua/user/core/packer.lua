28891bcb8b230ddf8bd1692d7a35a8a80ad426dd
BenjamimCS <uj1kjfomy@mozmail.com>
2023-03-25 03:56:21 -0400

    Reorder plugins list
    Reorder for better readability

--- lua/user/core/packer.lua
+++ lua/user/core/packer.lua
@@ -41,84 +41,96 @@ packer.init({
 packer.startup(function(use)
 
   -- As Dependecies
-  use "nvim-tree/nvim-web-devicons" -- optional, for file icons
+    -- for file icons
+    use "nvim-tree/nvim-web-devicons"
+    -- VSCode kind icons
+    use "onsails/lspkind.nvim"
 
-  use "wbthomason/packer.nvim" -- Have packer manage itself
-  use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
-  use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
+  -- Essential
+    use "wbthomason/packer.nvim" -- Have packer manage itself
+    use "nvim-lua/popup.nvim"    -- An implementation of the Popup API from vim in Neovim
+    use "nvim-lua/plenary.nvim"  -- Useful lua functions used ny lots of plugins
 
-  -- NvimTree
-  use {
-      "nvim-treesitter/nvim-treesitter",
-      run = function()
-          local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-          ts_update()
-      end,
-  }
+    -- Language server support
+    use "williamboman/mason.nvim" -- simple to use language server installer
+    use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+    use "neovim/nvim-lspconfig" -- enable LSP
 
-  -- Barbar
-  use {
-    "romgrk/barbar.nvim",
-    wants = "nvim-web-devicons"
-  }
+    -- Popup menu
+    -- cmp plugins
+    use "hrsh7th/nvim-cmp" -- The completion plugin
+    use "hrsh7th/cmp-buffer" -- buffer completions
+    use "hrsh7th/cmp-path" -- path completions
+    use "hrsh7th/cmp-cmdline" -- cmdline completions
+    use "hrsh7th/cmp-nvim-lsp"
+    use "hrsh7th/cmp-nvim-lua"
 
-  -- Indent Blankline
-  use "lukas-reineke/indent-blankline.nvim"
+  -- Productivity
+    -- An neovim explorer
+    use "nvim-tree/nvim-tree.lua"
 
-  -- Comment
-  use "numToStr/Comment.nvim"
+    -- Treesitter autotag for HTML
+    use "windwp/nvim-ts-autotag"
+    use "ziontee113/color-picker.nvim" -- pick colors in the terminal
+    -- Neovim line commenter
+    use "numToStr/Comment.nvim"
+    -- Multi cursor
+    use "mg979/vim-visual-multi"
+    -- Autopair for quote, braces etc.
+    use {
+      "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
+    -- Git integration for neovim
+    use "lewis6991/gitsigns.nvim"
+    -- Move lines
+    use "matze/vim-move"
+    -- Color code preview
+    use "norcalli/nvim-colorizer.lua"
 
-  -- Autopairs --
-  use {
-    "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-  }
+  -- Complements
+    -- Adds extra features for neovim
+    use {
+        "nvim-treesitter/nvim-treesitter",
+        run = function()
+            local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+            ts_update()
+        end,
+    }
+    -- Nice tabline, an listing buffers
+    use {
+      "romgrk/barbar.nvim",
+      wants = "nvim-web-devicons"
+    }
+    -- A good indentation level guide
+    use "lukas-reineke/indent-blankline.nvim"
 
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
+  -- Snippets
+    use "saadparwaiz1/cmp_luasnip" -- snippet completions
+    use {
+      "L3MON4D3/LuaSnip", --snippet engine
+      tag = "v<CurrentMajor>.*"
+    }
+    use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
-  -- snippets
-  use {
-    "L3MON4D3/LuaSnip", --snippet engine
-    tag = "v<CurrentMajor>.*"
-  }
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
+  -- Preview
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
 
-	-- LSP
-  use "williamboman/mason.nvim" -- simple to use language server installer
-  use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
-  use "neovim/nvim-lspconfig" -- enable LSP
+  -- Just beautifying it
+    use {
+      "nvim-lualine/lualine.nvim",
+      requires = { "kyazdani42/nvim-web-devicons", opt = true }
+    }
+    -- Colorschemes
+      -- A bunch of colorschemes you can try out use "lunarvim/darkplus.nvim"
+      -- use "lunarvim/colorschemes"
+      use "rafi/awesome-vim-colorschemes"
+      use "sam4llis/nvim-tundra"
+      use "ellisonleao/gruvbox.nvim"
 
-  use "lewis6991/gitsigns.nvim"
-
-  -- VSCode kind icons
-  use "onsails/lspkind.nvim"
-
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out use "lunarvim/darkplus.nvim"
-  use "nvim-tree/nvim-tree.lua"
-  use "rafi/awesome-vim-colorschemes"
-  use "sam4llis/nvim-tundra"
-  use "ellisonleao/gruvbox.nvim"
-
-  -- HTML utilities
-  use "windwp/nvim-ts-autotag"
-  -- CSS utilities
-  use "norcalli/nvim-colorizer.lua"
-  use "ziontee113/color-picker.nvim" -- pick colors in the terminal
-
-
-  -- Status Line ---
-  use {
-    "nvim-lualine/lualine.nvim",
-    requires = { "kyazdani42/nvim-web-devicons", opt = true }
-  }
 end)
 
 --- Include the plugins ---
